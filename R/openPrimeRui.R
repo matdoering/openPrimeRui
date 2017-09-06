1305b955486db7337cfb19e5fe385b411518eaf2
Matthias Doering <mdoering@mpi-inf.mpg.de>
2017-09-06 12:36:19 +0200

    added pkg Rmd entry

---
+++ R/openPrimeRui.R
@@ -0,0 +1,11 @@
+#' The openPrimeR Shiny Application.
+#'
+#' Using the openPrimeR Shiny application, you can evaluate existing
+#' primers or design novel primers for multiplex
+#' polymerase chain reaction that are optimized with respect
+#' to the coverage of template sequences and the physicochemical
+#' properties of the primers.
+#'
+#' The Shiny application can be started using \code{\link{startApp}}, 
+#' which should open the frontend in your default web browser.
+"_PACKAGE" 
